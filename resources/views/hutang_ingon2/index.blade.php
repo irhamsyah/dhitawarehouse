5df3ff90c60f36b7eb9a1d9953184cddf827ed51
Nur Qomari <nurqomari@gmail.com>
2025-09-18 15:02:18 +0700

    update create hutang

--- resources/views/hutang_ingon2/index.blade.php
+++ resources/views/hutang_ingon2/index.blade.php
@@ -45,7 +45,7 @@
                 @slot('tool')
                     <div class="box-tools">
                         <a class="tw-dw-btn tw-bg-gradient-to-r tw-from-indigo-600 tw-to-blue-500 tw-font-bold tw-text-white tw-border-none tw-rounded-full pull-right"
-                            href="{{ action([\App\Http\Controllers\SellController::class, 'create']) }}">
+                            href="{{ action([\App\Http\Controllers\HutangController::class, 'create']) }}">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                                 class="icon icon-tabler icons-tabler-outline icon-tabler-plus">
